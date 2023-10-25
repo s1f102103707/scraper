7db583d83df352c7a829c688b481b8ef93406ee1
s1f102103707 <s1f102103707@iniad.org>
2023-10-25 13:09:06 +0900

    a

--- src/sample.scraper.ts
+++ src/sample.scraper.ts
@@ -1,12 +1,19 @@
-import { chromium } from "playwright-core";
+import axios from "axios";
+import cheerio from "cheerio";
 
-(async () => {
-  const browser = await chromium.launch({
-    channel: "chrome",
-    headless: true,
-  });
-  const page = await browser.newPage();
-  await page.goto("https://www.google.co.jp/");
-  await page.screenshot({ path: `tmp/sample.png` });
-  await browser.close();
-})();
+async function fetchPageContent(url: string): Promise<string> {
+  const response = await axios.get(url);
+  return response.data;
+}
+
+const url = "https://db.netkeiba.com/horse/2019105219/";
+const html = await fetchPageContent(url);
+
+const $ = cheerio.load(html);
+
+// 例: ウェブページからタイトルを取得
+const pageTitle = $("title").text();
+console.log("ページタイトル:", pageTitle);
+
+const horseName = $("h1").text();
+console.log("競走馬の名前:", horseName);
